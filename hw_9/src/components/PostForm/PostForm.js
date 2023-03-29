bb6632166a346b5513974b4db0cf26e0d5057d87
o_ebrahim <kangurt.boy@gmail.com>
2023-03-29 17:48:44 +0500

    Реализация редактирование поста

---
+++ hw_9/src/components/PostForm/PostForm.js
@@ -0,0 +1,115 @@
+import React, { useState, useRef, useEffect } from 'react';
+const empty = {
+  id: 0,
+  author: {
+    avatar: 'https://alif-skills.pro/media/logo_alif.svg',
+    name: 'Alif Skills',
+  },
+  content: '',
+  photo: null,
+  hit: false,
+  likes: 0,
+  likedByMe: false,
+  hidden: false,
+  tags: [],
+  created: 0,
+};
+function PostForm({ onSave, postToEdit = empty }) {
+  const [post, setPost] = useState(empty);
+  const firstFocusEl = useRef(null);
+
+  const handleSubmit = (evt) => {
+    //отправка данных
+    evt.preventDefault();
+    const parsed =
+      post.tags
+        ?.map((o) => o.replace('#', ''))
+        .filter((o) => o.trim() !== '') || [];
+    const tags = parsed.length > 0 ? parsed : null;
+    const photo = { url: post.photo?.url, alt: post.photo?.alt || '' };
+    onSave({
+      ...post,
+      id: postToEdit?.id ? postToEdit.id : Date.now(),
+      created: Date.now(),
+      tags,
+      photo: !photo.url ? null : photo,
+    });
+    setPost(empty);
+    firstFocusEl.current.focus();
+  };
+  const handleChange = (evt) => {
+    // обработчик ввода
+    const { name, value } = evt.target;
+    if (name === 'tags') {
+      setPost((prevState) => ({ ...prevState, [name]: value.split(' ') }));
+      return;
+    }
+
+    if (name === 'photo') {
+      setPost((prevState) => {
+        const nextState = { ...prevState };
+        if (nextState.photo) {
+          nextState.photo.url = value;
+          return nextState;
+        }
+        nextState.photo = { url: value };
+        return nextState;
+      });
+      return;
+    }
+    if (name === 'alt') {
+      setPost((prevState) => {
+        const nextState = { ...prevState };
+
+        if (nextState.photo) {
+          nextState.photo.alt = value;
+          return nextState;
+        }
+        nextState.photo = { alt: value };
+        return nextState;
+      });
+      return;
+    }
+
+    setPost((prevState) => ({ ...prevState, [name]: value }));
+  };
+  
+  useEffect(() => {
+    if (postToEdit) {
+      setPost(postToEdit);
+      return;
+    }
+  }, [postToEdit]);
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea
+        name="content"
+        placeholder="content"
+        value={post?.content || ''}
+        onChange={handleChange}
+        ref={firstFocusEl}
+      ></textarea>
+      <input
+        name="tags"
+        placeholder="tags"
+        value={post?.tags?.join(' ') || ''}
+        onChange={handleChange}
+      ></input>
+      <input
+        name="photo"
+        placeholder="photo"
+        value={post?.photo?.url || ''}
+        onChange={handleChange}
+      ></input>
+      <input
+        name="alt"
+        placeholder="alt"
+        value={post?.photo?.alt || ''}
+        onChange={handleChange}
+      ></input>
+      <button>Ok</button>
+    </form>
+  );
+}
+
+export default PostForm;
---
+++ hw_9/src/components/Wall/Wall.js
@@ -0,0 +1,102 @@
+import Post from './../Post/Post';
+import PostForm from '../PostForm/PostForm';
+import React, { useState } from 'react';
+
+function Wall() {
+  const [posts, setPosts] = useState([
+    {
+      id: 2,
+      author: {
+        avatar: 'https://alif-skills.pro/media/logo_alif.svg',
+        name: 'Alif Skills',
+      },
+      content: 'Ну как, вы справились с домашкой?',
+      photo: null,
+      hit: true,
+      likedByMe: true,
+      likes: 222,
+      tags: ['deadline', 'homework'],
+      created: 1603774800,
+      hidden: false,
+    },
+    {
+      id: 1,
+      author: {
+        id: 1,
+        avatar: 'https://alif-skills.pro/media/logo_alif.svg',
+        name: 'Alif Skills',
+      },
+      content: null,
+      photo: {
+        url: 'https://alif-skills.pro/media/meme.jpg',
+        alt: 'Мем про дедлайн',
+      },
+      hit: true,
+      likes: 10,
+      likedByMe: true,
+      created: 1603501200,
+      hidden: true,
+    },
+  ]);
+  const [selecTedPost, setSelectedPost] = useState(null);
+  const handlePostRemove = (postID) => {
+    setPosts((prev) => {
+      return prev.filter((prevPost) => postID !== prevPost.id);
+    });
+  };
+  const handleHidde = (postID) => {
+    setPosts((prevState) => {
+      return prevState.map((item) => {
+        const post = item;
+        const hidden = !post.hidden;
+
+        if (post.id === postID) {
+          return { ...post, hidden };
+        }
+        return item;
+      });
+    });
+  };
+
+  const handleSave = (post) => {
+    setPosts((prev) => {
+      const newState = [...prev];
+      const exitedPost = prev.find((item) => item.id === post.id);
+
+      if (exitedPost) {
+        const result = newState.map((item) => {
+          if (item.id === post.id) {
+            item = post;
+          }
+          return item;
+        });
+        return [...result];
+      }
+      return [{ ...post }, ...prev];
+    });
+    setSelectedPost(null);
+  };
+  const handleEdit = (post) => {
+    setSelectedPost(() => {
+      const nextState = posts.find((item) => item.id === post);
+      return nextState;
+    });
+  };
+  return (
+    <>
+      <PostForm onSave={handleSave} postToEdit={selecTedPost} />
+      {posts.map((post, id) => (
+        <div key={id}>
+          <Post
+            post={post}
+            onRemove={handlePostRemove}
+            onHide={handleHidde}
+            onEdit={handleEdit}
+          />
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default Wall;
